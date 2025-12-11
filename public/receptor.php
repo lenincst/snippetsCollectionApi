4d273871ecb595fd81ee75276738fa4844551234
Lenin Costa da Rosa <lenincostta@gmail.com>
2025-12-10 22:28:10 -0300

    adicionado receptor

---
+++ public/receptor.php
@@ -0,0 +1,14 @@
+<?php
+ require_once __DIR__ . '/../server/config.php';
+ require_once __DIR__ . '/../server/crest-master/src/crest.php';
+
+$dados = $_REQUEST;
+
+$dados_log = print_r($dados, true);
+
+$log = date('d/m/Y H/i:s').": ".PHP_EOL;
+$log .= $dados_log.PHP_EOL;
+
+file_put_contents(__DIR__ . '/logs.txt', $log, FILE_APPEND);
+
+?>
