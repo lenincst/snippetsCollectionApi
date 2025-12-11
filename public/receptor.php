8bc895ed77acd9faa2e6474a63e0c1d17d390dd5
Lenin Costa <lenincostta@gmail.com>
2025-12-11 17:42:31 -0300

    atualizando

--- public/receptor.php
+++ public/receptor.php
@@ -5,11 +5,34 @@
 $dados = $_REQUEST;
 
 $dados_log = print_r($dados, true);
+$tipo_de_chamado = $dados['event'];
 
-$log = "//----------------------------------------------------//".date('d/m/Y H:i:s').": ".PHP_EOL;
-$log .= $dados_log.PHP_EOL."//----------------------------------------------------//";
+$log = "\n//********//\n".date('d/m/Y H:i:s').": ".PHP_EOL;
+$log .= $dados_log.PHP_EOL;
 
-file_put_contents(__DIR__ . '/logs.txt', $log, FILE_APPEND);
+file_put_contents(__DIR__ . '/logs.json', $log, FILE_APPEND);
+
+//----------------------------------------------------------------
+
+if ($tipo_de_chamado == 'ONCRMDEALUPDATE' or $tipo_de_chamado == 'ONCRMDEALADD'){
+    $id_negocio = $dados['data']['FIELDS']['ID'];
+
+    $dados_negocio = CRest::call(
+    'crm.deal.get',
+    [
+        'ID' => $id_negocio,
+        ]
+    
+    );
+
+    $dados_negocio = print_r($dados_negocio, true);
+
+
+
+    file_put_contents(__DIR__ .'/negocio.json', $dados_negocio.PHP_EOL, FILE_APPEND);
+
+}elseif($tipo_de_chamado == 'ONCRMDEALDELETE'){
+}
 
 echo "To aqui!";
 
